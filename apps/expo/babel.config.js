10c2eaab7f0c25a1a9474b886c2624240b29d869
oveigam <oveigam@gmail.com>
2022-09-21 09:12:22 +0200

    expo environment

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    plugins: ["nativewind/babel"],
+    plugins: ["nativewind/babel", "transform-inline-environment-variables"],
     presets: ["babel-preset-expo"],
   };
 };
--- apps/expo/src/utils/trpc.tsx
+++ apps/expo/src/utils/trpc.tsx
@@ -6,20 +6,26 @@ import { createTRPCReact } from "@trpc/react";
  */
 export const trpc = createTRPCReact<AppRouter>();
 
+const ENVIRONMENT = process.env.ENVIRONMENT;
+
 /**
  * Extend this function when going to production by
  * setting the baseUrl to your production API URL.
  */
 import Constants from "expo-constants";
 const getBaseUrl = () => {
-  /**
-   * Gets the IP address of your host-machine. If it cannot automatically find it,
-   * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
-   * you don't have anything else running on it, or you'd have to change it.
-   */
-  const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
-  if (!localhost) throw new Error("failed to get localhost, configure it manually");
-  return `ws://${localhost}:4000`;
+  if (ENVIRONMENT === "dev") {
+    /**
+     * Gets the IP address of your host-machine. If it cannot automatically find it,
+     * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
+     * you don't have anything else running on it, or you'd have to change it.
+     */
+    const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
+    if (!localhost) throw new Error("failed to get localhost, configure it manually");
+    return `ws://${localhost}:4000`;
+  } else {
+    return `wss://football-guess-production.up.railway.app`;
+  }
 };
 
 const wsClient = createWSClient({
