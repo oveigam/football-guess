4a5e8fdad81606173e4a21ff37ba80ebde4078c6
oveigam <oveigam@gmail.com>
2022-09-23 13:20:19 +0200

    server url

--- apps/nextjs/src/utils/trpc.ts
+++ apps/nextjs/src/utils/trpc.ts
@@ -4,21 +4,15 @@ import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
 import { createWSClient, wsLink } from "@trpc/client/links/wsLink";
 import { setupTRPC } from "@trpc/next";
 
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
-  return `http://localhost:${process.env.PORT ?? 4000}`; // dev SSR should use localhost
-};
-
 function getEndingLink() {
   if (typeof window === "undefined") {
     return httpBatchLink({
-      url: `http://localhost:4000/api/trpc`,
+      url: `http://${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
     });
   }
+
   const client = createWSClient({
-    url: "ws://localhost:4000",
+    url: `ws://${process.env.NEXT_PUBLIC_SERVER_URL}`,
   });
   return wsLink<AppRouter>({
     client,
@@ -28,7 +22,6 @@ function getEndingLink() {
 export const trpc = setupTRPC<AppRouter>({
   config() {
     return {
-      url: `${getBaseUrl()}/api/trpc`,
       links: [getEndingLink()],
       transformer,
     };
