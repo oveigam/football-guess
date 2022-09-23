4c6324c3bf5fe571de3212b433092ba94d115013
oveigam <oveigam@gmail.com>
2022-09-23 13:26:17 +0200

    favicon and head

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,10 +1,19 @@
-// src/pages/_app.tsx
-import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
+import "../styles/globals.css";
 import { trpc } from "../utils/trpc";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Fooball Guess</title>
+        <meta name="description" content="Football guessing game" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
